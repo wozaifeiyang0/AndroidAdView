0085bb4a5cff62c22de287789bd0c3d4a611fa3c
tanghy <wozaifeiyang0@163.com>
2015-01-29 20:14:00 +0800

    添加生成快捷方式的代码

---
+++ src/com/youyoucy/tanghy/core/WebViewOperation.java
@@ -0,0 +1,46 @@
+package com.youyoucy.tanghy.core;
+
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+/**
+ * Created by IBM on 2015/1/29.
+ */
+public class WebViewOperation {
+
+    private WebView webView;
+    private View pro;
+    public WebViewOperation(WebView paramView,View progress){
+
+        this.pro = progress;
+        webView = paramView;
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.requestFocus();
+        webView.setWebViewClient(new MyWebClient());
+        webView.setScrollBarStyle(View.SCROLLBARS_OUTSIDE_OVERLAY);
+
+    }
+
+    /**
+     * 加载页面
+     * @param url
+     */
+    public void load(String url){
+        webView.loadUrl(url);
+    }
+
+    private class MyWebClient extends WebViewClient {
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            if (pro !=null)
+            pro.setVisibility(View.GONE);
+        }
+    }
+
+}
+
+
